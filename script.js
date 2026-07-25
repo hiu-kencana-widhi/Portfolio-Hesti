700b06860e120327bc7c6ea77d6157ff94b74271
Hiu Kencana Widhi <widhi.limited@gmail.com>
2026-07-25 20:06:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
        ========================================================================== */
     const themeToggleBtn = document.getElementById('theme-toggle');
     
-    // Check local storage for theme, default to 'dark' on first access
-    const savedTheme = localStorage.getItem('theme') || 'dark';
+    // Check local storage for theme, default to 'light' on first access
+    const savedTheme = localStorage.getItem('theme') || 'light';
     
     if (savedTheme === 'dark') {
         document.body.classList.add('dark-theme');
